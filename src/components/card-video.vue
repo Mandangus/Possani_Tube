031edc1e5c010ddfaa80a209b6821e1658259278
Rafael-Corona <61752350+Rafael-Corona@users.noreply.github.com>
2022-06-25 17:26:55 -0300

    Card-video agora é 100% responsivo

--- src/components/card-video.vue
+++ src/components/card-video.vue
@@ -1,42 +1,50 @@
 <template>
 
 <div class="container shadow-lg py-4">
-     <div class="row justify-content-md-center">
-        <div class="col col-sm-auto">
-            <div class="card border-0" style="width: 55rem;">
-                <h3 class="card-title">{{aula.nome}}</h3>
-                <div class="card-body">
-                    <img class="img-fluid" id="videothumb" :src="aula.image"/>   
-                </div>
-            </div>
+    <div class="row justify-content-md-center">
+        <div class="col">
+            <h3>{{aula.nome}}</h3>
         </div>
     </div>
-    <div class="row justify-content-md-center">
-        <div class="col-md-auto">     
-            <div class="card border-dark p-2" id="descript" style="width: 25rem; height: 15rem;">
+
+    <div class="row">
+        <div class="col iframe-container mx-2">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Utj5xUmUEvk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+
+
+    <div class="row justify-content-center" align="center">
+        <div class="col- col-lg-5">     
+            <div class="card border-dark p-2 mt-4" id="descript" style="width: 25rem; height: 15rem;">
                 <h5 class="card-title">Descrição</h5>
                 <p class="card-text" style="line-height: 230%;">{{aula.description}}</p>
             </div>
         </div>
-        <div class="col-md-auto">
-            <div class="row">
-                <div class="col-7 g-3">
-                    <h3 id="preco">{{aula.price}} </h3> 
-                </div>
-                <div class="col g-2">
-                    <button class="btn btn-primary btn-lg" type="submit">
-                        <img :src="cartIcon" width="35" />
-                        Comprar
-                    </button>
-                </div>
+        <div class="col- col-lg-5 ">
+            <div class="card border-0" style="width: 25rem;"> 
+                <div class="row mt-4">
+                    <div class="col">
+                        <h3 id="preco">{{aula.price}} </h3> 
+                    </div>
+                    <div class="col">
+                        <button class="btn btn-primary btn-lg" type="submit">
+                            <img :src="cartIcon" width="35" />
+                            Comprar
+                        </button>
+                    </div>
 
-            </div>     
-            <div class="card border-0 my-3" style="width: 25rem;">
-                <p class="card-text">{{aula.detalhes}}</p>
+
+                </div>
+                <div class="row mt-4">
+                    <div class="col">     
+                        <p>{{aula.detalhes}}</p>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
-   
 </div>
 
 
@@ -83,5 +91,18 @@ export default{
     font-weight: bolder;
 }
 
+.iframe-container{
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%; 
+  height: 0;
+}
+.iframe-container iframe{
+  position: absolute;
+  top:0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 
 </style>
