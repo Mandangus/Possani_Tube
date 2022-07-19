ba6fffdf440423be3c68abd2f149633cdfdb6ec9
Rafael-Corona <61752350+Rafael-Corona@users.noreply.github.com>
2022-07-19 18:15:28 -0300

    Update new-productform.vue

--- Frontend/src/components/new-productform.vue
+++ Frontend/src/components/new-productform.vue
@@ -8,6 +8,9 @@
         <label>Preço do produto</label>
         <input type="text" class="form-control" placeholder="Preço do produto" v-model="preco">
         <br>
+        <label>Matéria do produto</label>
+        <textarea class="form-control"  placeholder="Insira aqui a matéria do produto" v-model="materia"></textarea>
+        <br>
         <label>Descrição do produto</label>
         <textarea class="form-control" rows="3" placeholder="Insira aqui a descrição do produto" v-model="descricao"></textarea>
         <br>
@@ -20,13 +23,22 @@
         <label>Link da CDN da prévia</label>
         <textarea class="form-control" placeholder="Insira aqui o link da CDN da prévia" v-model="previa"></textarea>
         <br>
+        <label>URL da thumbnail</label>
+        <textarea class="form-control" placeholder="Insira aqui a URL da thumbnail" v-model="thumbnail"></textarea>
+        <br>
     </div>
-    <button type="submit" class="btn btn-primary">Adicionar produto</button>
+    <button type="submit" class="btn btn-primary" @click="addVideo">Adicionar produto</button>
 </div>
 
 </template>
 
 <script>
+
+function getVal() {
+  const val = document.querySelector('input').value;
+  console.log(val);
+}
+
 export default {
     name: 'product-form',
     data() {
@@ -38,7 +50,34 @@ export default {
             video: "",
             previa: "",
         }
-    }
+    },
+
+    methods: {
+      addVideo: async function() {
+            try {
+                let route = "http://localhost:3000/product/"
+                fetch(route, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                    "nome": this.nome,
+                    "preco": this.preco,
+                    "materia": this.materia,
+                    "descricao": this.descricao,
+                    "detalhes": this.detalhes,
+                    "video": this.video,
+                    "preview": this.preview, 
+                    "vendas": 0,
+                    "thumbnail": this.thumbnail, 
+                    "active": true
+                  })
+                });	
+              }
+            catch (e) {alert("Error: " + e);}
+        }
+  }
 }
 </script>
 
